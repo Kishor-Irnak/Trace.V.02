85d8554c0b3b3cbfd95344c04c27abf1fcfaa437
Kishor Irnak <kishor@example.com>
2025-12-14 17:24:01 +0530

    bug fixed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
